e0af74284e8b978ac5a1a0ccc712d7dd3473993c
MauricioGut <maugutierrez@live.com.ar>
2023-09-27 10:12:56 -0300

    nueva iteracion

--- led1.ino
+++ led1.ino
@@ -14,10 +14,10 @@ void loop() {
   else{
       digitalWrite(13, HIGH);  // Enciende el LED
       //delay(1);
-      for(unsigned long i = 0; i < 1600000000UL; i++){}  // Simula un retraso de 1 milisegundo con un ciclo for vacío
+      for(unsigned long i = 0; i < 5347UL; i++){}  // Simula un retraso de 1 milisegundo con un ciclo for vacío
       digitalWrite(13, LOW);   // Apaga el LED                   
       //delay(1);
-      for(unsigned long i = 0; i < 1600000000UL; i++){}  // Simula un retraso de 1 milisegundo con un ciclo for vacío
+      for(unsigned long i = 0; i < 5347UL; i++){}  // Simula un retraso de 1 milisegundo con un ciclo for vacío
 		  
   }
 }
